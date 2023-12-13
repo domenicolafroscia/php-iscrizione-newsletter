6ffe8c9d50dd7dc828d6c92b201f9fb795c6c587
domenicolafroscia <domenicolafroscia@gmail.com>
2023-12-13 16:42:05 +0100

    add: error in page

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php if (str_contains($email, '@') !== false && str_contains($email, '.') !== false) { ?>
     <div class='alert alert-success'>Email valida!</div>
+    <?php header('Location: ./thankyou.php'); ?>
 <?php } else{ ?>
     <div class='alert alert-danger'>Email non valida!</div>
 <?php } ?>
---
+++ thankyou.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+
+
+if(!isset($_SESSION['auth']) || $_SESSION['auth'] !== true) {
+    header('Location: functions.php');
+    die;
+}   
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<div>
+    <h2>Grazie per aver inserito la tua email!</h2>
+</div>
+
+</body>
+</html>
